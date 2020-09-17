d922db89cf26627b16fd4c1433591893a1beb448
Michael Olsen <olsen3d@gmail.com>
2020-09-16 21:39:40 -0400

    calc interest

--- server/cron/calcInterest.js
+++ server/cron/calcInterest.js
@@ -3,10 +3,10 @@ const {Deposit} = require('../db/models')
 const colors = require('colors')
 
 const calcTest = () => {
-  console.log('running a cron job every 5 seconds'.bgBlue)
+  console.log('running a cron job every 5 seconds'.blue)
   cron.schedule('1,6,11,16,21,26,31,36,41,46,51,56 * * * * *', () => {
     const random = Math.floor(Math.random() * 1000)
-    console.log(`depositing ${random}`.bgBlue)
+    console.log(`depositing ${random}`.blue)
     Deposit.create({
       amount: random,
       accountId: '64a89a1b-19bc-4acf-af77-6415cdfabcf6'
